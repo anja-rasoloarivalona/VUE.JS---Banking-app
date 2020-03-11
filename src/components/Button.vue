97da363dda95ef1619f79486464eb178b2151346
anja <rasoloanja@gmail.com>
2020-03-11 13:20:49 -0400

    Created the button.vue component

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+    <router-link v-if="link" :to="'/' + link">
+        <slot></slot>
+    </router-link>
+    <button v-else @click="clicked" :type="type">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default {
+  props: {
+    link: String,
+    clicked: Function,
+    type: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>

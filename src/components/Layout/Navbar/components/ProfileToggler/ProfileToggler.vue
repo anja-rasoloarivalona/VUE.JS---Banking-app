cd5e7ce381326e882b6813996d3d6cc28bcea183
anja <rasoloanja@gmail.com>
2020-06-25 17:45:35 -0400

    Added switch toggler in navbar profile

--- src/components/Layout/Navbar/components/ProfileToggler/ProfileToggler.vue
+++ src/components/Layout/Navbar/components/ProfileToggler/ProfileToggler.vue
@@ -8,7 +8,13 @@
                 <ul class="profile__list" :style="{boxShadow: theme.isDark ? 'box-shadow: 1px 5px 12px -1px rgba(15,15,15,1)' : '1px 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'}">
                     <div class="profile__list__item">Account</div>
                     <div class="profile__list__item" @click="setDashboardIsBeingEditedToTrue">Edit Dashboard</div>
-                    <div class="profile__list__item" @click="toggleDarkMode">Dark theme</div>
+                    <div class="profile__list__item profile__list__item--theme">
+                        <span>Dark mode</span>
+                        <div class="profile__list__item--theme__switch" @click="toggleDarkMode" :class="{active: theme.isDark}">
+                            <div class="profile__list__item--theme__switch__bar"></div>
+                            <div class="profile__list__item--theme__switch__toggle"></div>
+                        </div>
+                    </div>
                     <div class="profile__list__item profile__list__item--logout" @click="$store.dispatch('logout')">
                         <span>Log out</span>
                         <app-icon name="logout" color="dark" size="large"/>
@@ -57,7 +63,7 @@ export default {
         background: $color-white;
         top: 3rem;
         left: -7rem;
-        width: 20rem;
+        width: 23rem;
         position: absolute;
         border-radius: .5rem;
         padding-top: 1rem;
@@ -72,9 +78,57 @@ export default {
                 display: flex;
                 align-items: center;
                 justify-content: space-between;
-                // border-top: 1px solid var(--lineColor);
+                // margin-top: 2rem;
+                // margin-bottom: 1rem;
+                border-top: 1px solid #ece9e9;
                 &:hover {
 
+                }
+            }
+            &--theme {
+                // background: blue;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                &__switch {
+                    background: #f5f5f5;
+                    height: 1.5rem;
+                    width: 5rem;
+                    position: relative;
+                    border-radius: 1.5rem;
+                    cursor: pointer;
+                    box-shadow: inset 0 3px 10px rgba(0, 0, 0, 0.247);
+                    &__bar {
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: 0%;
+                        height: 100%;
+                        border-radius: 1.5rem;
+                        background: var(--mainColor--dark);
+                        transition: all .3s ease-in;
+                    }
+                    &__toggle {
+                        background: var(--mainColor);
+                        position: absolute;
+                        top: 0;
+                        bottom: 0;
+                        left: 0;
+                        margin: auto;
+                        width: 2rem;
+                        height: 2rem;
+                        // box-shadow: -1px 0px 5px 1px rgba(37, 37, 37, 0.829);
+                        border-radius: 50%;
+                        transition: all .3s ease-in;
+                    }
+                    &.active {
+                        & .profile__list__item--theme__switch__toggle {
+                            transform: translateX(3rem);
+                        }
+                        & .profile__list__item--theme__switch__bar {
+                            width: 100%;
+                        }
+                    }
                 }
             }
         }
